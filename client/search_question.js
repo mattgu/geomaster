349152f58099698ac88bb54d5191f7023d5feddf
Adrian Jaeger <jgradrian@gmail.com>
2012-12-11 11:56:05 +0100

    asdasd

--- client/search_question.js
+++
@@ -1,8 +0,0 @@
-Template.search_users.events = {
-    'keyup #search-query': function(e) {
-        var $target = $(e.target),
-            value = $target.val();
-
-        Session.set('searchQuery', value);
-    }
-};
--- model/model.js
+++
@@ -1,4 +0,0 @@
-////////// Shared code (client and server) //////////
-
-Questions = new Meteor.Collection("questions");
-Players = new Meteor.Collection("players");
